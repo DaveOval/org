fa1c29bb9a0b7f0183ce14bb0ce02942da06515e
David Vazquez <dave_u@outlook.com>
2023-04-05 23:43:55 -0600

    se crearon mas partes del formulario

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,4 +1,9 @@
-function header(){
-    return <img src='/img/header.png' alt='Org'/>
-    
+import './header.css'
+
+function Header(){
+    return  <header className='header'>
+            <img src='/img/header.png' alt='Org'/>
+        </header>
 }
+
+export default Header
